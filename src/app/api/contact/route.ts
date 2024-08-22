3d7d138a5caaa5b1a3777f7c9220d1eab7321b2b
HariAditya2k3 <hariadityak03@example.com>
2024-08-22 23:28:24 +0530

    username update

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,10 +1,10 @@
-// src/app/api/contact/route.ts
+
 import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
-// Configure your SMTP transport
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // or another email service
+  service: 'gmail', 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -13,17 +13,21 @@ const transporter = nodemailer.createTransport({
 
 export async function POST(request: Request) {
   try {
-    const { email, query } = await request.json();
+  
+    const { email, query, username } = await request.json();
+
 
     const mailOptions = {
       from: email,
       to: process.env.EMAIL_USER,
       subject: 'Contact Form Submission',
-      text: `Query: ${query}`,
+      text: `Username: ${username}\nQuery: ${query}`,
     };
 
+   
     await transporter.sendMail(mailOptions);
 
+   
     return NextResponse.json({ message: 'Email sent successfully!' }, { status: 200 });
   } catch (error) {
     console.error('Error sending email:', error);
