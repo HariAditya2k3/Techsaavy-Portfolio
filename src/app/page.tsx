99024c81c0e3f0f3fa32c30ce8063fc88bd2ab1d
HariAditya2k3 <hariadityak03@example.com>
2024-10-04 22:12:25 +0530

    week7 code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
   
   {/* Button for Challenge - Responsive and below the poster */}
   <div className="flex justify-center mt-4">
-    <a href="https://www.hackerrank.com/weekly-code-challenge-second-years-week-6">
+    <a href="https://www.hackerrank.com/weekly-code-challenge-second-years-week-7">
       <button className="bg-blue-500 text-white px-4 py-2 text-base md:text-lg lg:text-xl rounded-lg hover:bg-blue-600 hover:shadow-lg focus:outline-none transition-all duration-300 ease-in-out">
         Start Challenge
       </button>
